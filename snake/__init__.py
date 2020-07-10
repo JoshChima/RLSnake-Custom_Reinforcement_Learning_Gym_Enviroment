a0d2d2b5b346e4406406b1444c3333e322299720
JoshChima <45519648+JoshChima@users.noreply.github.com>
2020-07-10 11:26:23 -0500

    NOFrameskip

--- snake/__init__.py
+++ snake/__init__.py
@@ -3,5 +3,11 @@ from gym.envs.registration import register
 register(
     id='SelfSnake-v0',
     entry_point='snake.envs:SnakeEnv',
-    max_episode_steps=1000,
+    max_episode_steps=1000000,
+)
+
+register(
+    id='SelfSnakeNoFrameskip-v0',
+    entry_point='snake.envs:SnakeEnv',
+    max_episode_steps=1000000,
 )
